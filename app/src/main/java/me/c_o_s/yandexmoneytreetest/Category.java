8cea8d3c5e41cdcae8205f7064dec5db6b6a1027
Constantine Colotiline <constantine.colotiline@live.com>
2015-08-05 23:07:05 +0300

    Added first implementation.

---
+++ app/src/main/java/me/c_o_s/yandexmoneytreetest/Category.java
@@ -0,0 +1,14 @@
+package me.c_o_s.yandexmoneytreetest;
+
+import android.support.annotation.Nullable;
+
+import java.util.List;
+
+/**
+ * Created by Constantine on 8/5/2015.
+ */
+public class Category {
+    public int id;
+    public String title;
+    public List<Category> subs;
+}
