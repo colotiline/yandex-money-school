23a1c5871ec406c44837c59d3620b714e1b7f93b
Constantine Colotiline <constantine.colotiline@live.com>
2015-08-12 21:20:20 +0300

    Added sqlite store. Added pull to refresh.

--- app/src/main/java/me/c_o_s/yandexmoneytreetest/model/Category.java
+++ app/src/main/java/me/c_o_s/yandexmoneytreetest/model/Category.java
@@ -6,6 +6,7 @@ import java.util.List;
  * Created by Constantine on 8/5/2015.
  */
 public class Category {
+    public long _id;
     public int id;
     public String title;
     public List<Category> subs;
